b45eea992cc0624f07ba9ae974ecb65b74c7aa30
ulhakim <61617529+ulhakim@users.noreply.github.com>
2020-04-18 15:53:57 +0900

    Update ForgotPasswordController.php

--- blog/app/Http/Controllers/Auth/ForgotPasswordController.php
+++ blog/app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
 class ForgotPasswordController extends Controller
+{
+    /*
     |--------------------------------------------------------------------------
     | Password Reset Controller
     |--------------------------------------------------------------------------
